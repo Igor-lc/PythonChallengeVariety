a200f159bae8e4effc8e526cc733ea7e5fb1cdb0
Igor <105611667+Igor-lc@users.noreply.github.com>
2023-12-24 12:51:19 -0800

    Add files via upload

---
+++ PythonicMemoryVariation&TextualSegmentation.py
@@ -0,0 +1,23 @@
+# ver without split
+import sys
+
+text = 'This Python course is unique ' * 1000
+memory_without_split = sys.getsizeof(text.encode('utf-8'))
+
+memory_without_split = sum(sys.getsizeof(word.encode('utf-8')) for word in text)
+print(f"memory_without_split: {memory_without_split} bytes")  # вывод: 952000 bytes
+
+
+# ver with split
+import sys
+
+text = 'This Python course is unique ' * 1000
+memory_without_split = sys.getsizeof(text.encode('utf-8'))
+
+memory_with_split = sum(sys.getsizeof((word + ' ').encode('utf-8')) for word in text.split())
+print(f"memory_with_split: {memory_with_split} bytes")  # вывод: 160034 bytes
+
+
+text = 'This Python course is unique ' * 1000
+for word in text:
+    print(f'{word}: id: {id(word)}, hash: {hash(word)}')
